15527086bc817c7d4ddff8f9f0c49d040f7b75c2
Marvin <marvinraj77@gmail.com>
2024-10-06 03:50:13 +0800

    connected project to mongodb database

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const dotenv = require("dotenv");
+const connectDB = require('./config/db');
+
+dotenv.config();
 
 const app = express();
 const port = 5000;
@@ -8,5 +12,6 @@ app.get("/", (req, res) => {
 })
 
 app.listen(5000, () => {
+    connectDB();
     console.log("server started at http://localhost:5000");
 })
